737261e2f06e04906808eeb40fdda4814294bda3
MrkPagani <paganim17@gmail.com>
2019-09-02 23:46:55 +0700

    All feature with Intent

--- app/src/main/java/com/example/edugateguru/HomeActivity.java
+++ app/src/main/java/com/example/edugateguru/HomeActivity.java
@@ -1,14 +1,49 @@
 package com.example.edugateguru;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView menu_tugas,menu_about,menu_profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        menu_tugas = findViewById(R.id.menuTugas);
+        menu_tugas.setOnClickListener(this);
+        menu_about = findViewById(R.id.menuAbout);
+        menu_about.setOnClickListener(this);
+        menu_profil = findViewById(R.id.menuProfil);
+        menu_profil.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.menuAbout:
+                Intent about = new Intent(HomeActivity.this,AboutActivity.class);
+                startActivity(about);
+                break;
+            case R.id.menuProfil:
+                Intent profil = new Intent(HomeActivity.this,ProfileActivity.class);
+                startActivity(profil);
+                break;
+            case R.id.menuTugas:
+                Intent Tugas = new Intent(HomeActivity.this,AboutActivity.class);
+                startActivity(Tugas);
+                break;
+        }
+
     }
 }
+
--- app/src/main/java/com/example/edugateguru/TugasActivity.java
+++ app/src/main/java/com/example/edugateguru/TugasActivity.java
@@ -2,13 +2,28 @@ package com.example.edugateguru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TugasActivity extends AppCompatActivity {
+
+    Button btn_tugas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tugas);
+
+        btn_tugas = findViewById(R.id.btnTugas);
+        btn_tugas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TugasActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
