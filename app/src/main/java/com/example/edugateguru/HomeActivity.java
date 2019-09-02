577fa43ccfb0c50e6908d6f8a0738af475575258
vikoms <vikoms95@gmail.com>
2019-09-02 22:04:10 +0700

    nambah Fragment sama Intent

--- app/src/main/java/com/example/edugateguru/HomeActivity.java
+++ app/src/main/java/com/example/edugateguru/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.edugateguru;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.example.edugateguru.Fragment.FragmentHome;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        loadFragment(new FragmentHome());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_home,fragment).commit();
     }
 }
--- app/src/main/java/com/example/edugateguru/ProfileActivity.java
+++ app/src/main/java/com/example/edugateguru/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.edugateguru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.editProfile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileActivity.this, EditProfileActivity.class));
+            }
+        });
     }
 }
