fbd27e383b7f300fd29378eee8575578af45ca76
Viko Muhammad Saputra <vikoms95@gmail.com>
2020-01-16 10:12:47 +0700

    membuat fitur edit profile dan tambah tugas

--- app/src/main/java/com/example/edugateguru/EditProfileActivity.java
+++ app/src/main/java/com/example/edugateguru/EditProfileActivity.java
@@ -1,19 +1,36 @@
 package com.example.edugateguru;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.edugateguru.Models.Guru;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -28,8 +45,12 @@ public class EditProfileActivity extends AppCompatActivity {
     public static final String EXTRA_EMAIL = "extra_email";
     public static final String EXTRA_NIP = "extra_nip";
     public static final String EXTRA_PELAJARAN = "extra_pelajaran";
+    public static final int pReqCode = 1;
+    public static final int REQUESCODE = 1;
 
     private EditText edtNama, edtPhone, edtEmail, edtKota;
+    private ImageView imgProfile;
+    private Uri imgUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +66,7 @@ public class EditProfileActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edtEmail);
         edtKota = findViewById(R.id.edtKota);
         edtPhone = findViewById(R.id.edtTelphone);
+        imgProfile = findViewById(R.id.photo_profile);
 
         String nama = getIntent().getStringExtra(EXTRA_NAMA);
         String kota = getIntent().getStringExtra(EXTRA_KOTA);
@@ -66,20 +88,93 @@ public class EditProfileActivity extends AppCompatActivity {
                 String valKota = edtKota.getText().toString();
                 String valPelajaran = getIntent().getStringExtra(EXTRA_PELAJARAN);
                 String valNip = getIntent().getStringExtra(EXTRA_NIP);
-                updateData(valNama, valEmail, valPhone, valKota, valNip, valPelajaran);
+                updateData(valNama, valEmail, valPhone, valKota, valNip, valPelajaran, imgUri, mAuth.getCurrentUser());
             }
         });
+
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkAndRequestForPermissions();
+                } else {
+                    openGallery();
+                }
+            }
+        });
+
+
     }
 
-    private void updateData(String valNama, String valEmail, String valPhone, String valKota, String valNip, String valPelajaran) {
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("Guru").child(currentUser.getUid());
-        Guru guru = new Guru(valNama, valKota, valNip,valEmail,valPhone,valPelajaran);
-        databaseReference.setValue(guru);
-
-        Toast.makeText(this, "Data Berhasil di Update", Toast.LENGTH_SHORT).show();
-
-        startActivity(new Intent(EditProfileActivity.this, HomeActivity.class));
+    private void checkAndRequestForPermissions() {
+        if (ContextCompat.checkSelfPermission(EditProfileActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(EditProfileActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "Pleast accept for required permission", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(EditProfileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, pReqCode);
+            }
+        } else {
+            openGallery();
+        }
 
     }
 
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+    }
+
+    private void updateData(final String valNama, final String valEmail, final String valPhone, final String valKota, final String valNip, final String valPelajaran, Uri imgUri, final FirebaseUser currentUser) {
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("Guru").child(this.currentUser.getUid());
+        if (this.imgUri != null) {
+            StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+            final StorageReference imageFilePath = mStorage.child(this.imgUri.getLastPathSegment());
+            imageFilePath.putFile(this.imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            UserProfileChangeRequest updateProfile = new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(uri).build();
+
+                            currentUser.updateProfile(updateProfile).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Guru guru = new Guru(valNama, valKota, valNip, valEmail, valPhone, valPelajaran);
+                                    databaseReference.setValue(guru);
+
+                                    Toast.makeText(EditProfileActivity.this, "Update profile berhasil", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(EditProfileActivity.this, HomeActivity.class));
+                                    finish();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            Guru guru = new Guru(valNama, valKota, valNip, valEmail, valPhone, valPelajaran);
+            databaseReference.setValue(guru);
+
+            Toast.makeText(EditProfileActivity.this, "Update profile berhasil", Toast.LENGTH_SHORT).show();
+
+            startActivity(new Intent(EditProfileActivity.this, HomeActivity.class));
+            finish();
+        }
+
+//
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null) {
+            imgUri = data.getData();
+            imgProfile.setImageURI(imgUri);
+        }
+    }
 }
--- app/src/main/java/com/example/edugateguru/TugasActivity.java
+++ app/src/main/java/com/example/edugateguru/TugasActivity.java
@@ -7,28 +7,41 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.edugateguru.Models.Kelas;
 import com.example.edugateguru.Models.Tugas;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+
 public class TugasActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     Button btn_tugas;
     DatabaseReference ref;
-    String namaTugas,descTugas,dateTugas,timeTugas,kelasTugas;
-    EditText editNamaTugas,editDescTugas,editDateTugas,editTimeTugas,editKelasTugas;
+    String namaTugas, descTugas, dateTugas, timeTugas, kelasTugas;
+    EditText editNamaTugas, editDescTugas, editDateTugas, editTimeTugas, editKelasTugas;
     TextView tvDateTugas;
     ImageView dateChooser;
+    ArrayAdapter<Kelas> adapter;
+    ArrayList<Kelas> listKelas;
+    DatabaseReference refKelas;
+    Spinner spinnerKelas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +52,14 @@ public class TugasActivity extends AppCompatActivity implements DatePickerDialog
         editDescTugas = findViewById(R.id.descTugas);
         tvDateTugas = findViewById(R.id.textDate);
         editTimeTugas = findViewById(R.id.textTime);
-        editKelasTugas = findViewById(R.id.textKelas);
+        spinnerKelas = findViewById(R.id.spinner_kelas);
 
 
         ref = FirebaseDatabase.getInstance().getReference("Tugas");
+        refKelas = FirebaseDatabase.getInstance().getReference("Kelas");
+
+
+        listKelas = new ArrayList<>();
 
         btn_tugas = findViewById(R.id.btnTugas);
         btn_tugas.setOnClickListener(new View.OnClickListener() {
@@ -50,12 +67,13 @@ public class TugasActivity extends AppCompatActivity implements DatePickerDialog
             public void onClick(View view) {
                 namaTugas = editNamaTugas.getText().toString();
                 descTugas = editDescTugas.getText().toString();
-                dateTugas = editDateTugas.getText().toString();
+                dateTugas = tvDateTugas.getText().toString();
                 timeTugas = editTimeTugas.getText().toString();
-                kelasTugas= editKelasTugas.getText().toString();
+                Kelas kelas = (Kelas) spinnerKelas.getSelectedItem();
+                kelasTugas = kelas.getKeyKelas();
 
                 String id = ref.push().getKey();
-                Tugas tugas = new Tugas(namaTugas,descTugas,dateTugas,timeTugas,kelasTugas);
+                Tugas tugas = new Tugas(namaTugas, descTugas, dateTugas, timeTugas, kelasTugas);
 
                 ref.child(id).setValue(tugas);
                 Toast.makeText(TugasActivity.this, "Berhasil", Toast.LENGTH_SHORT).show();
@@ -74,16 +92,44 @@ public class TugasActivity extends AppCompatActivity implements DatePickerDialog
 
     public void showDatePickerDialog() {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
-                                            Calendar.getInstance().get(Calendar.YEAR),
-                                            Calendar.getInstance().get(Calendar.MONTH),
-                                            Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
     }
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-        month+=1;
-        String date = year + "/" + month + "/" +day ;
+        month += 1;
+        String date = year + "/" + month + "/" + day;
         tvDateTugas.setText(date);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        refKelas.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot item:dataSnapshot.getChildren()) {
+                    Kelas kelas = new Kelas(item.getKey(),item.getValue().toString());
+                    listKelas.add(kelas);
+                }
+               loadDataKelas();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void loadDataKelas() {
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,listKelas);
+
+        adapter.notifyDataSetChanged();
+        spinnerKelas.setAdapter(adapter);
     }
 }
