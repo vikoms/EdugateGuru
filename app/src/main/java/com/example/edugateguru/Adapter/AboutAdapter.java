ad65df0b417e45a8185c23d46d28c48a6eca0ba8
fajarlaksana <fajarlaksana13@gmail.com>
2020-01-16 12:47:18 +0700

    Membuat Fitur About

---
+++ app/src/main/java/com/example/edugateguru/Adapter/AboutAdapter.java
@@ -0,0 +1,64 @@
+package com.example.edugateguru.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.edugateguru.Models.About;
+import com.example.edugateguru.R;
+
+import java.util.List;
+
+public class AboutAdapter extends PagerAdapter {
+
+    private List<About> models;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public AboutAdapter(List<About> models, Context context) {
+        this.models = models;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_about, container, false);
+
+        ImageView imageView;
+        TextView title, desc;
+
+        imageView = view.findViewById(R.id.image);
+        title = view.findViewById(R.id.title);
+        desc = view.findViewById(R.id.desc);
+
+        imageView.setImageResource(models.get(position).getImage());
+        title.setText(models.get(position).getTitle());
+        desc.setText(models.get(position).getDesc());
+
+        container.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
